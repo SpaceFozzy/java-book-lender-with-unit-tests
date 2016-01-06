bd5128d8fcef97ab89bcaa46113f82f3cc6e3933
Robot Monster <jon@robotmonster.ca>
2016-01-06 16:10:19 -0500

    finished final lesson and built LibraryApplication.jar

--- Readme.md
+++ Readme.md
@@ -1,12 +1,13 @@
 #A Java Book-Lending Application Project (With Tests)
-This project is a result of my work going through [Mark Dexter's tutorial series on Eclipse and Java](https://www.youtube.com/playlist?list=PLUGinutrSzN97XcdPgNUI3HB9Tjwvdl_r). While I've done lots of web development work and obj c programming, this is the first time I've fired up Eclipse and tried to make something in Java. Also my first use of Test-Driven Development.
+This project is a result of my work going through [Mark Dexter's tutorial series on Eclipse and Java](https://www.youtube.com/playlist?list=PLUGinutrSzN97XcdPgNUI3HB9Tjwvdl_r). While I've done lots of web development work and obj c programming, this is the first time I've fired up Eclipse and tried to make something in Java. Also my first use of test-driven development.
 
-#Things I'm Loving About Eclipse
+#Progress: 16/16
+I've completed all the lessons in this tutorial series and created a small book-borrowing application using test-driven development. The final program is built in build/LibraryApplication.jar. There is no GUI, but when ran, the program will spit out a few library transactions and its status after each one.
+
+#Things I Loved About Eclipse
 * Eclipse autocomplete!
-* Really smart refactoring for renaming variables, classes, etc
+* Smart refactoring for renaming variables, classes, etc
 * Eclipse scrapbook for playing with chunks of blocks of code
-* Eclipse QuickFix makes it easy to write a failing test, then have Eclipse define the functions necessary for it to pass :D
+* QuickFix makes it easy to write a failing test, then have Eclipse define the functions necessary for it to pass :D
 * Extract Function makes it easy to remove duplicated code and replace it all with method calls to a single new function.
 
-#Progress: 15/16
-Wrote getAvailableBooks method to satisfy testGetAvailableBooks test. Added toString methods to Book and MyLibrary class. Finished Lesson 15.
--- src/org/libraryApplication/tutorial/MyLibrary.java
+++ src/org/libraryApplication/tutorial/MyLibrary.java
@@ -2,6 +2,8 @@ package org.libraryApplication.tutorial;
 
 import java.util.ArrayList;
 
+import junit.framework.TestListener;
+
 public class MyLibrary {
 
 	String name;
@@ -96,5 +98,59 @@ public class MyLibrary {
 		return this.getName()+": "+this.getBooks().size()+" books, "+this.getBorrowers().size()+" borrowers" ;
 	}
 	
+	public static void main(String[] args) {
+		//create a new MyLibrary
+		MyLibrary mainLibrary = new MyLibrary("Jon's Library");
+		
+		Book book1 = new Book("A Brave New World");
+		Book book2 = new Book("Paradise Lost");
+		book1.setAuthor("Aldous Huxley");
+		book2.setAuthor("John Milton");
+		mainLibrary.addBook(book1);
+		mainLibrary.addBook(book2);
+		
+		Person reid = new Person();
+		Person josh = new Person();
+		reid.setName("Reid");
+		josh.setName("Josh");
+		mainLibrary.addBorrower(reid);
+		mainLibrary.addBorrower(josh);
+		
+		System.out.println("New library created...");
+		mainLibrary.printStatus();
+		
+		System.out.println("Checking out Brave New World to Josh...\n");
+		mainLibrary.checkOutBook(book1, josh);
+		mainLibrary.printStatus();
+		
+//		System.out.println("Checking out Paradise Lost to Reid");
+//		mainLibrary.checkOutBook(book2, reid);
+		
+		System.out.println("Do some more stuff...\n");
+		mainLibrary.checkIn(book1);
+		mainLibrary.checkOutBook(book2,reid);
+		mainLibrary.printStatus();
+	}
+
+	private void printStatus() {
+		System.out.println("Status Report of " + this.getName() + "\n"
+				+ this.toString());
+		for (Book thisBook : this.getBooks()){
+			System.out.println(thisBook);
+		}
+		for (Person thisPerson : this.getBorrowers()){
+			int count = this.getBooksForPerson(thisPerson).size();
+			System.out.println(thisPerson.getName() + " has " + count
+					+ " books");
+		}
+		
+		System.out.println("Books Available: "+this.getAvailableBooks().size());
+		System.out.println("Books Borrowed: "+this.getUnavailableBooks().size());
+		
+		System.out.println("---- End of Status Report ----\n");
+		
+		
+		
+	}
 
 }
